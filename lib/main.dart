bb927d85accfa7cfcf56c475d20418ef59acf0d8
jacksos101 <jacksos101@gmail.com>
2019-05-15 14:50:15 +1200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: null, // TODO: Add title from
+    home: null, // TODO: Add home page class when built
+    theme: null // TODO: Add theme data
+  ));
+}
+
