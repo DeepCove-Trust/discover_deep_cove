0c4a0d88bba547b2b5d5ac4f2147708a180b2b3a
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2019-07-07 13:14:26 +1200

    Updated the main.dart
    has theme data
    routes
    imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hci_v2/util/hex_color.dart';
+import 'package:hci_v2/util/route_generator.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: null, // TODO: Add title from
-    home: null, // TODO: Add home page class when built
-    theme: null // TODO: Add theme data
-  ));
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(
+      MaterialApp(
+        theme: ThemeData(
+          //Green
+          primaryColor: HexColor("FF8BC34A"),
+          //Charcoal
+          primaryColorDark: HexColor("FF262626"),
+          //Orange
+          accentColor: HexColor("FFFF5026"),
+          //Dark Gray
+          backgroundColor: HexColor("FF363636"),
+
+          fontFamily: 'Roboto',
+
+          textTheme: TextTheme(
+            headline: TextStyle(fontSize: 30, color: Colors.white),
+            body1: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+        initialRoute: '/',
+        onGenerateRoute: RouteGenerator.generateRoute,
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  });
 }
-
