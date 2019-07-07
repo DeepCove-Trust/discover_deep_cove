3d7b94c90c3022cef578fb69a200e5e62faab1a2
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2019-07-07 13:16:20 +1200

    handles named routing
    and passing args

---
+++ lib/util/route_generator.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:hci_v2/data/sample_data_quiz.dart';
+import 'package:hci_v2/views/activites/count_view.dart';
+import 'package:hci_v2/views/activites/photograph_view.dart';
+import 'package:hci_v2/views/activites/picture_select_view.dart';
+import 'package:hci_v2/views/activites/picture_tap_view.dart';
+import 'package:hci_v2/views/activites/text_answer_view.dart';
+import 'package:hci_v2/views/fact_file/fact_file_details.dart';
+import 'package:hci_v2/views/home.dart';
+import 'package:hci_v2/views/quiz/quiz_questions.dart';
+import 'package:hci_v2/views/quiz/quiz_unlock.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    var args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) => Home(),
+        );
+
+      //Fact file routes
+      case '/factFileDetails':
+        if (args is FactFilesDetails) {
+          final FactFilesDetails args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (_) => FactFilesDetails(
+                  entry: args.entry,
+                  heroTag: args.heroTag,
+                ),
+          );
+        }
+
+        return _errorRoute();
+
+      //Quiz routes
+      case '/quizUnlock':
+        return MaterialPageRoute(
+          builder: (_) => QuizUnlock(),
+        );
+
+      case '/quizQuestions':
+        if (args is Quiz) {
+          return MaterialPageRoute(
+            builder: (_) => QuizQuestions(
+                  quiz: args,
+                ),
+          );
+        }
+
+        return _errorRoute();
+
+      //Activity routes
+      case '/countView':
+        if (args is CountView) {
+          final CountView args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (_) => CountView(
+                  activity: args.activity,
+                  fromMap: args.fromMap,
+                ),
+          );
+        }
+
+        return _errorRoute();
+
+      case '/photographView':
+        if (args is PhotographView) {
+          final PhotographView args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (_) => PhotographView(
+                  activity: args.activity,
+                  fromMap: args.fromMap,
+                ),
+          );
+        }
+
+        return _errorRoute();
+
+      case '/pictureSelectView':
+        if (args is PictureSelectView) {
+          final PictureSelectView args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (_) => PictureSelectView(
+                  activity: args.activity,
+                  fromMap: args.fromMap,
+                ),
+          );
+        }
+
+        return _errorRoute();
+
+      case '/pictureTapView':
+        if (args is PictureTapView) {
+          final PictureTapView args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (_) => PictureTapView(
+                  activity: args.activity,
+                  fromMap: args.fromMap,
+                ),
+          );
+        }
+
+        return _errorRoute();
+
+      case '/textAnswerView':
+        if (args is TextAnswerView) {
+          final TextAnswerView args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (_) => TextAnswerView(
+                  activity: args.activity,
+                  fromMap: args.fromMap,
+                ),
+          );
+        }
+
+        return _errorRoute();
+
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Text(
+            'ERROR',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      );
+    });
+  }
+}
