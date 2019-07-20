bca7ad14628e2cbfd8c105ffe9aafe17700f15b4
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2019-07-20 12:27:36 +1200

    Removed comments
    this has been verified on the frontEnd branch

--- lib/views/quiz/quiz_questions.dart
+++ lib/views/quiz/quiz_questions.dart
@@ -99,9 +99,8 @@ class _QuizQuestionsState extends State<QuizQuestions> {
     );
   }
 
-  //TODO figure out a better way to diff between text answer and image answer
-  //figure out a better way to handle answers when shuffled
   //TODO figure out a way to get the correct answer and the guess when answers is shuffled
+  //figure out a better way to handle answers when shuffled
 
   ///Returns the appropriate view to display depending on whether the [quiz] has ended or has more questions.
   Widget getQuestion() {
@@ -119,7 +118,6 @@ class _QuizQuestionsState extends State<QuizQuestions> {
         highscore: newHighscore,
       );
     } else if (widget.quiz.questions[index].image != null) {
-      //TODO: Verify that the appbar is hiding correctly
       if (widget.quiz.questions.length - index == 1)
         setState(() => appbarVisible = false);
 
