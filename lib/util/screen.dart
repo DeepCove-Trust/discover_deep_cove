1abd7639faf24b33e6788dee14529edfb5674d73
MitchellQ <mitchell.quarrie@outlook.com>
2019-07-29 22:02:01 +1200

    Update screen.dart
    - only percent named
    - removed handleOrientation method uneeded

--- lib/util/screen.dart
+++ lib/util/screen.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'handleOrientation.dart';
-
 class Screen {
-  static double width({BuildContext context, double percentage = 100}) =>
+  static double width(BuildContext context, {double percentage = 100}) =>
       MediaQuery.of(context).size.width / 100 * percentage;
 
-  static double height({BuildContext context, double percentage = 100}) =>
+  static double height(BuildContext context, {double percentage = 100}) =>
       MediaQuery.of(context).size.height / 100 * percentage;
 
   static Orientation orientation(BuildContext context) =>
       MediaQuery.of(context).orientation;
 
-  static setOrientations(BuildContext context) => Screen.width(context: context) >= 600
-      ? handleOrientation([
+  static setOrientations(BuildContext context) => Screen.width(context) >= 600
+      ? SystemChrome.setPreferredOrientations([
           DeviceOrientation.portraitUp,
           DeviceOrientation.landscapeLeft,
           DeviceOrientation.landscapeRight
         ])
-      : handleOrientation([DeviceOrientation.portraitUp]);
+      : SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   static bool isPortrait(BuildContext context) =>
       Screen.orientation(context) == Orientation.portrait;
