740c5dd74dffe9d5edb1d1af74a5caeaf124f5ea
MitchellQ <mitchell.quarrie@outlook.com>
2019-07-29 19:44:43 +1200

    Add method to get percent of screen x or y
    Also formatted

--- lib/util/screen.dart
+++ lib/util/screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Screen {
-  static double width(BuildContext context) => MediaQuery.of(context).size.width;
+  static double width(BuildContext context) =>
+      MediaQuery.of(context).size.width;
 
-  static double height(BuildContext context) =>MediaQuery.of(context).size.height;
+  static double height(BuildContext context) =>
+      MediaQuery.of(context).size.height;
 
-  static Orientation orientation(BuildContext context) => MediaQuery.of(context).orientation;
+  static Orientation orientation(BuildContext context) =>
+      MediaQuery.of(context).orientation;
+
+  static double percentOfWidth(BuildContext context, double percent) =>
+      MediaQuery.of(context).size.width -
+      (MediaQuery.of(context).size.width * (percent / 100));
+
+  static double percentofHeight(BuildContext context, double percent) =>
+      MediaQuery.of(context).size.height -
+      (MediaQuery.of(context).size.height * (percent / 100));
 }
