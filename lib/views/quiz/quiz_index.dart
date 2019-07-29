a88d9b880aa414b779e4a577906ad02b5573e744
MitchellQ <mitchell.quarrie@outlook.com>
2019-07-30 09:22:19 +1200

    Changed to 2 on portrait and 3 on landscape

--- lib/views/quiz/quiz_index.dart
+++ lib/views/quiz/quiz_index.dart
@@ -37,7 +37,7 @@ class _QuizIndexState extends State<QuizIndex> {
               mainAxisSpacing: Screen.width(context, percentage: 2.5),
               crossAxisSpacing: Screen.width(context, percentage: 2.5),
               crossAxisCount: (Screen.width(context) >= 600
-                  ? Screen.isPortrait(context) ? 3 : 2
+                  ? Screen.isPortrait(context) ? 2 : 3
                   : 1),
               padding: EdgeInsets.all(
                 Screen.width(context, percentage: 2.5),
