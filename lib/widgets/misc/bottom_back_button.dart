ba69e04a1a19e949c0723bf03cb25edfc6ed337b
MitchellQ <mitchell.quarrie@outlook.com>
2019-08-06 20:53:34 +1200

    changed height to be based off screen height

--- lib/widgets/misc/bottom_back_button.dart
+++ lib/widgets/misc/bottom_back_button.dart
@@ -1,3 +1,4 @@
+import 'package:discover_deep_cove/util/screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:discover_deep_cove/widgets/misc/text/body.dart';
@@ -10,7 +11,7 @@ class BottomBackButton extends StatelessWidget {
       child: FlatButton(
         onPressed: () => Navigator.pop(context),
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical : 20),
+          padding: EdgeInsets.symmetric(vertical : Screen.height(context, percentage: 1.5)),
           child: Body( 'Back'),
         )
       ),
