903dc97892432f16a070c416f7e4f80caf55cb44
MitchellQ <mitchell.quarrie@outlook.com>
2019-08-09 21:05:31 +1200

    formatting

--- lib/widgets/settings/settings_button.dart
+++ lib/widgets/settings/settings_button.dart
@@ -17,10 +17,9 @@ class SettingsButton extends StatelessWidget {
         padding: EdgeInsets.symmetric(
             vertical: Screen.height(context, percentage: 5), horizontal: 12),
         child: Row(
-          mainAxisAlignment:
-              !Screen.isPortrait(context)
-                  ? MainAxisAlignment.center
-                  : MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: !Screen.isPortrait(context)
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.spaceEvenly,
           children: [
             Transform.scale(
               scale: 1.5, // Todo: Is this different to setting icon size?
