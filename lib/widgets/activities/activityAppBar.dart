c78f05c0a67da24a3476ebefeaecbb2eced7a05c
MitchellQ <mitchell.quarrie@outlook.com>
2019-08-22 21:01:53 +1200

    overide size on heading widget

--- lib/widgets/activities/activityAppBar.dart
+++ lib/widgets/activities/activityAppBar.dart
@@ -1,3 +1,4 @@
+import 'package:discover_deep_cove/util/screen.dart';
 import 'package:discover_deep_cove/widgets/misc/text/heading.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ class ActivityAppBar extends StatelessWidget with PreferredSizeWidget{
       leading: Container(),
       title: Heading(
         text,
+        size: Screen.width(context) >= 600 ? 30 : 20,
       ),
       centerTitle: true,
       backgroundColor: Theme.of(context).primaryColor,
