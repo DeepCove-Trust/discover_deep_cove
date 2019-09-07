2d866691aac4869c043d015c8028b8c0f5aa1855
MitchellQ <mitchell.quarrie@outlook.com>
2019-09-07 14:34:19 +1200

    improved the widget added the surrounding widgets
    and a size

--- lib/widgets/misc/image_source.dart
+++ lib/widgets/misc/image_source.dart
@@ -1,22 +1,35 @@
 import 'package:discover_deep_cove/util/hex_color.dart';
-import 'package:discover_deep_cove/util/screen.dart';
 import 'package:flutter/material.dart';
 
 class ImageSource extends StatelessWidget {
   final String source;
   final bool isCopyright;
+  final double size;
 
-  ImageSource({this.source, this.isCopyright});
+  ImageSource({this.source = "", this.isCopyright = false, this.size = 16});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      isCopyright ? "©" : "" + source,
-      style: TextStyle(
-        color: HexColor("FF999999"),
-        fontStyle: FontStyle.italic,
-        fontSize: (Screen.isSmall(context) ? 16 : 20),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                isCopyright ? "© " + source : source,
+                style: TextStyle(
+                  color: HexColor("FF999999"),
+                  fontStyle: FontStyle.italic,
+                  fontSize: size,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
