21e7c605d823ed1edbed9b3e450afc10b1f602c3
jacksos101 <jacksos101@gmail.com>
2019-11-22 18:22:20 +1300

    Improve activity unlock view

--- lib/views/activites/activity_unlock.dart
+++ lib/views/activites/activity_unlock.dart
@@ -2,6 +2,7 @@ import 'package:discover_deep_cove/util/screen.dart';
 import 'package:discover_deep_cove/widgets/misc/bottom_back_button.dart';
 import 'package:discover_deep_cove/widgets/misc/text/body_text.dart';
 import 'package:discover_deep_cove/widgets/misc/text/heading.dart';
+import 'package:discover_deep_cove/widgets/misc/text/sub_heading.dart';
 import 'package:flutter/material.dart';
 
 class ActivityUnlock extends StatefulWidget {
@@ -42,143 +43,112 @@ class _ActivityUnlockState extends State<ActivityUnlock> {
 
   buildContent(BuildContext context) {
     return (Screen.isTablet(context) && !Screen.isPortrait(context))
-        ? GridView.count(
-            crossAxisCount: 2,
-            children: [
-              getBottomHalf(context),
-              getTopHalf(context),
+        ? SingleChildScrollView(
+            child: Row(
+            children: <Widget>[
+              Expanded(child: Column(children: buildQRExample(),)),
+              Expanded(child: buildInputs())
             ],
-          )
+          ))
         : SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                getTopHalf(context),
-                getBottomHalf(context),
+                buildInputs(),
+                SizedBox(height: 30),
+                ...buildQRExample(),
               ],
             ),
           );
   }
 
-  getTopHalf(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: Screen.isTablet(context) ? 28.0 : 8.0,
-            vertical: Screen.width(context,
-                percentage: Screen.isPortrait(context) ? 15 : 5),
-          ),
-          child: Container(
-            width: Screen.width(context,
-                percentage: Screen.width(context) < 600 ? 100 : 80),
-            color: Theme.of(context).primaryColor,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: Screen.width(context, percentage: 5),
-                    bottom: Screen.width(context, percentage: 5),
-                  ),
-                  child: Heading(
-                    "Enter QR unlock code:",
-                    size: Screen.width(context) >= 600
-                        ? 30
-                        : Screen.width(context) <= 350 ? 16 : 20,
-                  ),
-                ),
-                Transform.scale(
-                  scale: Screen.isTablet(context) ? 1.25 : 1,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Container(
-                      width: Screen.width(
-                        context,
-                        percentage: Screen.isTablet(context) ? 30 : 62.5,
-                      ),
-                      color: Colors.white,
-                      child: TextField(
-                        focusNode: _textFieldFocus,
-                        controller: textController,
-                        keyboardType: TextInputType.phone,
-                        style: TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                          hintText: 'Enter code...',
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.all(8.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: Screen.width(context, percentage: 5),
-                  ),
-                  child: OutlineButton(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Heading(
-                        "Unlock",
-                        size: Screen.width(context) >= 600
-                            ? 30
-                            : Screen.width(context) <= 350 ? 16 : 20,
-                      ),
-                    ),
-                    onPressed: () => verifyCode(context),
-                    borderSide: BorderSide(
-                      color: Color(0xFFFFFFFF),
-                    ),
-                  ),
-                ),
-              ],
+  buildInputs() {
+    return Container(
+      width: 1500, // will not overflow
+      height: Screen.isLandscape(context)
+          ? Screen.height(context) - 50
+          : Screen.height(context) * 0.4,
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              top: Screen.width(context, percentage: 5),
+              bottom: Screen.width(context, percentage: 5),
+            ),
+            child: Heading(
+              "Enter QR unlock code:",
+              size: Screen.width(context) >= 600
+                  ? 30
+                  : Screen.width(context) <= 350 ? 16 : 20,
             ),
           ),
-        ),
-      ],
+          Transform.scale(
+            scale: Screen.isTablet(context) ? 1.25 : 1,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Container(
+                width: Screen.width(
+                  context,
+                  percentage: Screen.isLandscape(context) ? 30 : 60,
+                ),
+                color: Colors.white,
+                child: TextField(
+                  focusNode: _textFieldFocus,
+                  controller: textController,
+                  keyboardType: TextInputType.phone,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: InputDecoration(
+                    hintText: 'Enter code...',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(8.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: Screen.width(context, percentage: 5),
+            ),
+            child: OutlineButton(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Heading(
+                  "Unlock",
+                  size: Screen.width(context) >= 600
+                      ? 30
+                      : Screen.width(context) <= 350 ? 16 : 20,
+                ),
+              ),
+              onPressed: () => verifyCode(context),
+              borderSide: BorderSide(
+                color: Color(0xFFFFFFFF),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  getBottomHalf(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: Screen.width(context) <= 350 ? 20 : 50),
-              child: BodyText(
-                "Example QR code.",
-                size: Screen.width(context) > 600 ? 30 : null,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  vertical: Screen.height(context, percentage: 2),
-                  horizontal: Screen.width(context) <= 350 ? 20 : 50),
-              child: Container(
-                width: Screen.width(context,
-                    percentage: Screen.width(context) <= 350
-                        ? 50
-                        : !Screen.isPortrait(context) ? 40 : 60),
-                height: Screen.width(context,
-                    percentage: Screen.width(context) <= 350
-                        ? 50
-                        : !Screen.isPortrait(context) ? 40 : 60),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  image: DecorationImage(
-                    image: AssetImage('assets/invalidQRcode.png'),
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-            ),
-          ],
+  buildQRExample() {
+    return <Widget>[
+      Container(
+        height: Screen.isLandscape(context)
+            ? Screen.height(context, percentage: 60)
+            : Screen.width(context, percentage: 60),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          image: DecorationImage(
+            image: AssetImage('assets/invalidQRcode.png'),
+            fit: BoxFit.contain,
+          ),
         ),
-      ],
-    );
+      ),
+      SizedBox(height: 25),
+      SubHeading('Example QR Code')
+    ];
   }
 }
