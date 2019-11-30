29762f1982bc3dc09ae18823d8bce8183867f6ff
jacksos101 <jacksos101@gmail.com>
2019-11-30 16:17:34 +1300

    Add VerboseState class
    This class prints is lifecycle changes to the console for easier debugging.

---
+++ lib/util/verbose_state.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class VerboseState<T extends StatefulWidget> extends State<T> {
+  String name;
+
+  VerboseState(){
+    name = this.runtimeType.toString();
+  }
+
+  @override
+  @mustCallSuper
+  Widget build(BuildContext context){
+      print('$name built');
+  }
+
+  @override
+  @mustCallSuper
+  void deactivate() {
+    print('$name deactivated');
+    super.deactivate();
+  }
+
+  @override
+  @mustCallSuper
+  void didUpdateWidget(StatefulWidget oldWidget) {
+    print('$name updated');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  @mustCallSuper
+  void didChangeDependencies() {
+    print('$name changed dependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  @mustCallSuper
+  void dispose() {
+    print('$name disposed');
+    super.dispose();
+  }
+
+  @override
+  @mustCallSuper
+  void reassemble() {
+    print('$name reassembled');
+    super.reassemble();
+  }
+
+  @override
+  @mustCallSuper
+  void initState() {
+    print('$name initialized');
+    super.initState();
+  }
+
+
+}
