456288671b78e93f7450ab85839bc8306996e6cb
MitchellQ <mitchell.quarrie@outlook.com>
2022-08-21 11:35:41 +1200

    Update database_adapter.dart
    Update to relative import
    Re ordered imports
    Formatting
    Migrate ancestorWidgetOfExactType to findAncestorWidgetOfExactType

--- lib/data/database_adapter.dart
+++ lib/data/database_adapter.dart
@@ -1,22 +1,24 @@
-import 'package:discover_deep_cove/data/db.dart';
-import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
 import 'package:jaguar_query_sqflite/jaguar_query_sqflite.dart';
+
+import 'db.dart';
 
 /// This widget serves as a container widget which should be placed high up
 /// in the widget tree. Descendant widgets can use the [of] method to access
 /// the adapter object of this widget.
 class DatabaseAdapter extends StatelessWidget {
-  DatabaseAdapter({Key key, @required this.adapter, @required this.child})
-      : super(key: key);
+  const DatabaseAdapter({
+    Key key,
+    @required this.adapter,
+    @required this.child,
+  }) : super(key: key);
 
   final SqfliteAdapter adapter;
   final Widget child;
 
   /// Returns the adapter to the database
   static SqfliteAdapter of(BuildContext context) {
-    DatabaseAdapter provider =
-        context.ancestorWidgetOfExactType(DatabaseAdapter);
+    DatabaseAdapter provider = context.findAncestorWidgetOfExactType<DatabaseAdapter>();
     return provider.adapter;
   }
 
